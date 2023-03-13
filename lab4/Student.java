b72c3ef74c02b08ebfdbd009a646d003e78aa2a2
fphillips22 <fphillips1209@gmail.com>
2023-03-13 15:40:52 -0600

    test commit

--- lab4/Student.java
+++ lab4/Student.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 public class Student {
     private String name;
     private int idNumber;
